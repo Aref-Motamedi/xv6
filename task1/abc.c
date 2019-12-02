c4a08c1bd8890276e7ffd17a9e784ea8c0c72bb6
Hosein <hoseinbeheshtifard@gmail.com>
2019-12-02 00:56:46 -0800

    update task1 by Hosein

--- task1/abc.c
+++ task1/abc.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
 #include <sys/types.h> 
 #include <unistd.h> 
+#include<string.h>
 
+int babyfunc(char number[]){
+        int sumeven = 0;
+        int i;
+    for(i = 0 ; i < strlen(number) ; i++)
+    {
+        if(number[i] % 2 == 0)
+        sumeven = sumeven + number[i]-48;
+        
+    }
+
+    return sumeven;
+}
 
 int main()
 {
     pid_t pid = fork();
+    int p1[2];
+    pipe(p1);
     if (pid==0){
         // wait ta baba bede
+      // printf("sum even digits is = %d",babyfunc(number));
+
 
     }else if (pid>0){
-        int number;
-        scanf("%d",&number);
+        char number[1024];
+        scanf("%s", number);
         // numbero bede bache
         //wait ta bache bede
-    }
+
     return 0;
 }
-
-int babyfunc(int number){
-    
-    return sumeven
 }
+
