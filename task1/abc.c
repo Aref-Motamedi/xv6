3296e4b920b4fb4cf39acbe079eb7e1e6e9a812d
Aref <aref78.m@gmail.com>
2019-12-03 07:30:08 -0800

    piping in Q1 is done!

--- task1/abc.c
+++ task1/abc.c
@@ -4,8 +4,8 @@
 #include<string.h>
 
 int babyfunc(char number[]){
-        int sumeven = 0;
-        int i;
+    int sumeven = 0;
+    int i;
     for(i = 0 ; i < strlen(number) ; i++)
     {
         if(number[i] % 2 == 0)
@@ -18,21 +18,57 @@ int babyfunc(char number[]){
 
 int main()
 {
-    pid_t pid = fork();
     int p1[2];
-    pipe(p1);
+    int p2[2];
+    if (pipe(p1)<0 || pipe(p2)<0){
+        printf("failed to pipe\n");
+        return 0;
+    }
+
+    pid_t pid = fork();
+    
     if (pid==0){
-        // wait ta baba bede
-      // printf("sum even digits is = %d",babyfunc(number));
 
+        close(p1[1]); //close the write side of pipe 1
 
+        char str[1024];
+        read(p1[0],str,1024); // getting the number from parent
+        
+        int number = babyfunc(str); // getting the sum of even digits
+        
+        char newstr[128] = "The sum of even digits in the input number: ";
+
+        // *** HOSEIN CODE HERE! ***
+        
+        close(p2[0]); // close the read side of pipe 2
+        write(p2[1],newstr,128); // passing the result
+        close(p2[1]); // close the write side of pipe 2
+        
+        return 0;
+        
     }else if (pid>0){
+        
+        // parent process
+
         char number[1024];
         scanf("%s", number);
-        // numbero bede bache
-        //wait ta bache bede
+        
+        close(p1[0]); // closing the read side of pipe 1
+        
+        write(p1[1], number, strlen(number)+1); // sending number
+        close(p1[1]); // closing the write side of pipe 1
+        
 
-    return 0;
-}
+        close(p2[1]); // closing the write side of pipe 2
+
+        char str[128];
+        read(p2[0],str,128); // getting the result from child
+        
+        close(p2[0]); //closing the read side of pipe 2
+        
+        printf("%s\n",str);
+
+        return 0;  
+    }
 }
 
